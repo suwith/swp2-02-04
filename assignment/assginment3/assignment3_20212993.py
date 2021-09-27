92826d7b8f9b85fd038f7f7eacd0d58ab0771fd4
박상범 <90344951+sangbeomP@users.noreply.github.com>
2021-09-28 01:57:07 +0900

    Update assignment3_20212993.py

--- assignment/assginment3/assignment3_20212993.py
+++ assignment/assginment3/assignment3_20212993.py
@@ -36,7 +36,7 @@ def doScoreDB(scdb):
             record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3], }
             scdb += [record]
         elif parse[0] == 'del':
-            for p in scdb[:]: # for문으로 반복하는 리스트를 삭제하면 한 칸씩 앞으로 당겨짐 -> 리스트 내포를 사용
+            for p in scdb[:]: 
                 if p['Name'] == parse[1]:
                     scdb.remove(p)
         elif parse[0] == 'show':
