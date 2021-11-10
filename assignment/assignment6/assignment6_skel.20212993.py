cc6c722f7bfd28996522eec3d0616ad8b52e19ab
박상범 <90344951+sangbeomP@users.noreply.github.com>
2021-11-11 02:28:42 +0900

    Create assignment6_skel.20212993.py

---
+++ assignment/assignment6/assignment6_skel.20212993.py
@@ -0,0 +1,114 @@
+import pickle
+import sys
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+    QHBoxLayout, QVBoxLayout, QApplication, QLabel,
+    QComboBox, QTextEdit, QLineEdit )
+from PyQt5.QtCore import Qt
+
+
+class ScoreDB(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.dbfilename = 'assignment6.dat'
+        self.scoredb = []
+        self.readScoreDB()
+        self.showScoreDB()
+
+    def initUI(self):
+        name = QLabel('Name:')
+        age = QLabel('Age:')
+        score = QLabel('Score:')
+        amount = QLabel('Amount:')
+        result = QLabel('Result:')
+        key = QLabel('Key:')
+
+        self.nameEdit = QLineEdit()
+        self.ageEdit = QLineEdit()
+        self.scoreEdit = QLineEdit()
+        self.amountEdit = QLineEdit()
+        self.keyEdit = QComboBox()
+        self.resultEdit = QTextEdit()
+
+        self.keyEdit.addItem('Name')
+        self.keyEdit.addItem('Age')
+        self.keyEdit.addItem('Score')
+
+        addButton = QPushButton("Add")
+        delButton = QPushButton("Del")
+        findButton = QPushButton("Find")
+        incButton = QPushButton("Inc")
+        showButton = QPushButton("Show")
+
+        resultView = QTextEdit()
+
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+
+        hbox.addStretch(1)
+        hbox.addWidget(name)
+        hbox.addWidget(self.nameEdit)
+        hbox.addWidget(age)
+        hbox.addWidget(self.ageEdit)
+        hbox.addWidget(score)
+        hbox.addWidget(self.scoreEdit)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addStretch(1)
+        hbox2.addWidget(amount)
+        hbox2.addWidget(self.amountEdit)
+        hbox2.addWidget(key)
+        hbox2.addWidget(self.keyEdit)
+
+        hbox3 = QHBoxLayout()
+        hbox3.addStretch(1)
+        hbox3.addWidget(addButton)
+        hbox3.addWidget(delButton)
+        hbox3.addWidget(findButton)
+        hbox3.addWidget(incButton)
+        hbox3.addWidget(showButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addWidget(result)
+        vbox.addWidget(self.resultEdit)
+
+        addButton.clicked.connect(self.addButtonClicked)
+        delButton.clicked.connect(self.delButtonClicked)
+        findButton.clicked.connect(self.findButtonClicked)
+        incButton.clicked.connect(self.incButtonClicked)
+        showButton.clicked.connect(self.showButtonClicked)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 500, 250)
+        self.setWindowTitle('Assignment6')
+        self.show()
+
+        def closeEvent(self, event):
+            
+
+        def readScoreDB(self):
+            
+
+
+        def writeScoreDB(self):
+            
+        def showScoreDB(self):
+            
+
+        def addButten(self):
+            
+
+        def delButten(self):
+            
+
+        def incButten(self):
+            
+
+        def findButten(self):
+            
